9109d4ac260b18fa67eaef3ee03a205ad367d058
JCIII <jcostello711@gmail.com>
2022-05-14 11:36:07 -0400

    done

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,53 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Nav() {
-    const categories = [
-        {
-            name: "commercial",
-            description:
-                "Photos of grocery stores, food trucks, and other commercial projects",
-        },
-        { name: "portraits", description: "Portraits of people in my life" },
-        { name: "food", description: "Delicious delicacies" },
-        {
-            name: "landscape",
-            description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
-    ];
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+        contactSelected,
+        setContactSelected
+    } = props;
 
-    function categorySelected() {
-        console.log(` clicked`)
-    }
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
 
     return (
-        <header>
-            <h2>
-                <a data-testid="link" href="/">
-                <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+        <header className="flex-row px-1">
+        <h2>
+            <a data-testid="link" href="/">
+            <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+            </a>
+        </h2>
+        <nav>
+            <ul className="flex-row">
+            <li className="mx-2">
+                <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+                About me
                 </a>
-            </h2>
-            <nav>
-                <ul className="flex-row">
-                <li className="mx-2">
-                    <a data-testid="about" href="#about">
-                    About me
-                    </a>
-                </li>
-                <li>
-                    <span>Contact</span>
-                </li>
-                {categories.map((category) => (
+            </li>
+            <li
+                className={`mx-1 ${
+                    currentCategory.name === category.name && !contactSelected && `navActive`
+                    }`}
+                key={category.name}
+            >
+                <span onClick={() => setContactSelected(true)}>Contact</span>
+            </li>
+            {categories.map((category) => (
                 <li
-                    className="mx-1"
-                    key={category.name}
+                className={`mx-1 ${
+                    currentCategory.name === category.name && 'navActive'
+                    }`}
+                key={category.name}
                 >
-                    <span onClick={() => categorySelected(category.name)} >
-                    {category.name}
-                    </span>
+                <span
+                    onClick={() => {
+                    setCurrentCategory(category);
+                    setContactSelected(false);
+                    }}
+                >
+                    {capitalizeFirstLetter(category.name)}
+                </span>
                 </li>
-                ))}
-                </ul>
-            </nav>
+            ))}
+            </ul>
+        </nav>
         </header>
     );
 }
